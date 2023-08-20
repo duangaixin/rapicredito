cc476fcd798587eab377634a5078aadbe478d750
段改新 <289307564@qq.com>
2023-08-20 09:13:17 +0800

    main create

--- lib/page/auth/contact/auth_contact_state.dart
+++ lib/page/auth/contact/auth_contact_state.dart
@@ -1,7 +1,17 @@
-
 import 'package:get/get.dart';
 
 class AuthContactState {
+  ///关系1
+  final RxString _relationshipOne = ''.obs;
 
+  String get relationshipOne => _relationshipOne.value;
 
+  set relationshipOne(value) => _relationshipOne.value = value;
+
+  ///关系2
+  final RxString _relationshipTwo = ''.obs;
+
+  String get relationshipTwo => _relationshipTwo.value;
+
+  set relationshipTwo(value) => _relationshipTwo.value = value;
 }
--- lib/page/auth/contact/auth_contact_view.dart
+++ lib/page/auth/contact/auth_contact_view.dart
@@ -11,11 +11,99 @@ import 'package:rapicredito/widget/custom_select_view.dart';
 class AuthContactPage extends GetView<AuthContactCtr> {
   const AuthContactPage({Key? key}) : super(key: key);
 
+  Widget get oneContactView => Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const CommonAuthTitleView(
+            title: 'Primera referencia personal',
+          ),
+          Obx(() {
+            return CustomSelectView(
+              editTitle: 'Relación',
+              hintText: 'Por favor elige',
+              editContent: controller.state.relationshipOne,
+              action: () {},
+            );
+          }),
+          CustomEditView(
+            controller: TextEditingController(),
+            editTitle: 'Número de celular',
+            hintText: 'Introducir texto',
+          ),
+          CustomEditView(
+            controller: TextEditingController(),
+            editTitle: 'Nombre',
+            hintText: 'Introducir texto',
+          ),
+        ],
+      );
+
+  Widget get twoContactView => Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            padding: const EdgeInsets.only(top: 33.0, bottom: 8.0),
+            child: const CommonAuthTitleView(
+              title: 'Segunda referencia personal',
+            ),
+          ),
+          Obx(() {
+            return CustomSelectView(
+              editTitle: 'Relación',
+              hintText: 'Por favor elige',
+              editContent: controller.state.relationshipTwo,
+              action: () {},
+            );
+          }),
+          CustomEditView(
+            controller: TextEditingController(),
+            editTitle: 'Número de celular',
+            hintText: 'Introducir texto',
+          ),
+          CustomEditView(
+            controller: TextEditingController(),
+            editTitle: 'Nombre',
+            hintText: 'Introducir texto',
+          ),
+        ],
+      );
+
+  Widget get bottomView => Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 39.0, right: 39.0, top: 26.0),
+            child: CustomColorButton(
+              disableColors: const [
+                Color(0xffF5F6F4),
+                Color(0xffF5F6F4),
+              ],
+              disable: false,
+              colors: const [Color(0xffB8EF17), Color(0xffB8EF17)],
+              height: 46.0,
+              borderRadius: BorderRadius.circular(8.0),
+              btnContent: const Text(
+                'Registrarse',
+                style: TextStyle(
+                    fontSize: 15.0,
+                    color: Color(0xff333333),
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(top: 30.0, bottom: 20.0),
+            child: CommonAuthAgreeView(),
+          )
+        ],
+      );
+
   @override
   Widget build(BuildContext context) {
-    double bottom = MediaQuery.of(context).viewInsets.bottom;
-    print('$bottom----build----duanxin');
-
+    //  double bottom = MediaQuery.of(context).viewInsets.bottom;
     return CustomPageBgView(
         title: 'Contacto de emergencia',
         content: Container(
@@ -25,147 +113,80 @@ class AuthContactPage extends GetView<AuthContactCtr> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisSize: MainAxisSize.max,
-                children: [
-                  const CommonAuthTitleView(
-                    title: 'Primera referencia personal',
-                  ),
-                  CustomSelectView(
-                    editTitle: 'Relación',
-                    hintText: 'Por favor elige',
-                    editContent: '',
-                    action: () {},
-                  ),
-                  CustomEditView(
-                    controller: TextEditingController(),
-                    editTitle: 'Número de celular',
-                    hintText: 'Introducir texto',
-                  ),
-                  CustomEditView(
-                    controller: TextEditingController(),
-                    editTitle: 'Nombre',
-                    hintText: 'Introducir texto',
-                  ),
-                  Container(
-                    padding: EdgeInsets.only(top: 33.0, bottom: 8.0),
-                    child: const CommonAuthTitleView(
-                      title: 'Segunda referencia personal',
-                    ),
-                  ),
-                  CustomSelectView(
-                    editTitle: 'Relación',
-                    hintText: 'Por favor elige',
-                    editContent: '',
-                    action: () {},
-                  ),
-                  CustomEditView(
-                    controller: TextEditingController(),
-                    editTitle: 'Número de celular',
-                    hintText: 'Introducir texto',
-                  ),
-                  CustomEditView(
-                    controller: TextEditingController(),
-                    editTitle: 'Nombre',
-                    hintText: 'Introducir texto',
-                  ),
-                  Padding(
-                    padding:
-                        EdgeInsets.only(left: 39.0, right: 39.0, top: 26.0),
-                    child: CustomColorButton(
-                      disableColors: const [
-                        Color(0xffF5F6F4),
-                        Color(0xffF5F6F4),
-                      ],
-                      disable: false,
-                      colors: const [Color(0xffB8EF17), Color(0xffB8EF17)],
-                      height: 46.0,
-                      borderRadius: BorderRadius.circular(8.0),
-                      btnContent: const Text(
-                        'Registrarse',
-                        style: TextStyle(
-                            fontSize: 15.0,
-                            color: Color(0xff333333),
-                            fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.only(top: 30.0, bottom: 20.0),
-                    child: CommonAuthAgreeView(),
-                  )
-                ],
+                children: [oneContactView, twoContactView, bottomView],
               ),
             )));
 
-    return CustomPageBgView(
-        title: 'Contacto de emergencia',
-        content: Container(
-          padding: const EdgeInsets.only(left: 14.0, right: 16.0, top: 18.0),
-          child: Stack(
-            children: [
-              Positioned(
-                child: SingleChildScrollView(
-                  physics: BouncingScrollPhysics(),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.max,
-                    children: [
-                      const CommonAuthTitleView(
-                        title: 'Primera referencia personal',
-                      ),
-                      CustomSelectView(
-                        editTitle: 'Relación',
-                        hintText: 'Por favor elige',
-                        editContent: '',
-                        action: () {},
-                      ),
-                      CustomEditView(
-                        controller: TextEditingController(),
-                        editTitle: 'Número de celular',
-                        hintText: 'Introducir texto',
-                      ),
-                      CustomEditView(
-                        controller: TextEditingController(),
-                        editTitle: 'Nombre',
-                        hintText: 'Introducir texto',
-                      ),
-                      SizedBox(
-                        height: 33.0,
-                      ),
-                      const CommonAuthTitleView(
-                        title: 'Segunda referencia personal',
-                      ),
-                      CustomSelectView(
-                        editTitle: 'Relación',
-                        hintText: 'Por favor elige',
-                        editContent: '',
-                        action: () {},
-                      ),
-                      CustomEditView(
-                        controller: TextEditingController(),
-                        editTitle: 'Número de celular',
-                        hintText: 'Introducir texto',
-                      ),
-                      CustomEditView(
-                        controller: TextEditingController(),
-                        editTitle: 'Nombre',
-                        hintText: 'Introducir texto',
-                      ),
-                    ],
-                  ),
-                ),
-                bottom: bottom > 0 ? 5.0 : 120,
-                left: 0.0,
-                right: 0.0,
-                top: 0.0,
-              ),
-              Positioned(
-                child: CommonAuthAgreeView(),
-                bottom: bottom > 0 ? -115.0 : 20.0,
-                left: 0.0,
-                right: 0.0,
-              )
-            ],
-          ),
-        ));
+    // return CustomPageBgView(
+    //     title: 'Contacto de emergencia',
+    //     content: Container(
+    //       padding: const EdgeInsets.only(left: 14.0, right: 16.0, top: 18.0),
+    //       child: Stack(
+    //         children: [
+    //           Positioned(
+    //             child: SingleChildScrollView(
+    //               physics: BouncingScrollPhysics(),
+    //               child: Column(
+    //                 crossAxisAlignment: CrossAxisAlignment.start,
+    //                 mainAxisSize: MainAxisSize.max,
+    //                 children: [
+    //                   const CommonAuthTitleView(
+    //                     title: 'Primera referencia personal',
+    //                   ),
+    //                   CustomSelectView(
+    //                     editTitle: 'Relación',
+    //                     hintText: 'Por favor elige',
+    //                     editContent: '',
+    //                     action: () {},
+    //                   ),
+    //                   CustomEditView(
+    //                     controller: TextEditingController(),
+    //                     editTitle: 'Número de celular',
+    //                     hintText: 'Introducir texto',
+    //                   ),
+    //                   CustomEditView(
+    //                     controller: TextEditingController(),
+    //                     editTitle: 'Nombre',
+    //                     hintText: 'Introducir texto',
+    //                   ),
+    //                   SizedBox(
+    //                     height: 33.0,
+    //                   ),
+    //                   const CommonAuthTitleView(
+    //                     title: 'Segunda referencia personal',
+    //                   ),
+    //                   CustomSelectView(
+    //                     editTitle: 'Relación',
+    //                     hintText: 'Por favor elige',
+    //                     editContent: '',
+    //                     action: () {},
+    //                   ),
+    //                   CustomEditView(
+    //                     controller: TextEditingController(),
+    //                     editTitle: 'Número de celular',
+    //                     hintText: 'Introducir texto',
+    //                   ),
+    //                   CustomEditView(
+    //                     controller: TextEditingController(),
+    //                     editTitle: 'Nombre',
+    //                     hintText: 'Introducir texto',
+    //                   ),
+    //                 ],
+    //               ),
+    //             ),
+    //             bottom: bottom > 0 ? 5.0 : 120,
+    //             left: 0.0,
+    //             right: 0.0,
+    //             top: 0.0,
+    //           ),
+    //           Positioned(
+    //             child: CommonAuthAgreeView(),
+    //             bottom: bottom > 0 ? -115.0 : 20.0,
+    //             left: 0.0,
+    //             right: 0.0,
+    //           )
+    //         ],
+    //       ),
+    //     ));
   }
 }
