94af0f06ae9c539ae1b9bb2431c41dc49613b664
段改新 <289307564@qq.com>
2023-08-23 14:47:20 +0800

    client

--- lib/page/main/home/widget/home_loan_audit_view.dart
+++ lib/page/main/home/widget/home_loan_audit_view.dart
@@ -4,8 +4,8 @@ import 'package:rapicredito/page/main/home/index.dart';
 import 'package:rapicredito/page/main/home/widget/common_agreement_view.dart';
 import 'package:rapicredito/page/main/home/widget/common_app_bar_view.dart';
 import 'package:rapicredito/page/main/home/widget/common_loan_money_view.dart';
-import 'package:rapicredito/style/index.dart';
-import 'package:rapicredito/widget/custom_image_view.dart';
+import 'package:rapicredito/utils/string_ext.dart';
+
 
 class HomeLoanAuditView extends GetView<MainHomeCtr> {
   const HomeLoanAuditView({Key? key}) : super(key: key);
@@ -28,7 +28,7 @@ class HomeLoanAuditView extends GetView<MainHomeCtr> {
                     title: 'Bajo Revisión',
                   ),
                   const CommonLoanMoneyView(),
-                  dealTipView,
+                  auditTipView,
                   const Spacer(),
                   const Padding(
                     padding: EdgeInsets.only(bottom: 30.0),
@@ -41,33 +41,19 @@ class HomeLoanAuditView extends GetView<MainHomeCtr> {
     });
   }
 
-  Widget get dealTipView => Container(
+  Widget get auditTipView => Container(
       margin: const EdgeInsets.only(left: 16.0, right: 16.0, top: 29.0),
-      padding:
-          const EdgeInsets.only(left: 3.0, right: 8.0, top: 9.0, bottom: 10.0),
+      padding: const EdgeInsets.only(
+          left: 23.0, right: 22.0, top: 16.0, bottom: 15.0),
       decoration: BoxDecoration(
           color: const Color(0xffF9F9F9),
           borderRadius: BorderRadius.circular(8.0)),
-      child: const Row(
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          CustomImageView(
-            Resource.assetsImageAuthFace,
-            imageType: ImageType.assets,
-            width: 66.0,
-            height: 66.0,
-          ),
-          Expanded(
-              child: Padding(
-            padding: EdgeInsets.only(left: 11.0),
-            child: Text(
-              'Su solicitud ha sido aprobado.\nSu préstamo está llegando en su cuenta.',
-              style: TextStyle(
-                fontSize: 14.0,
-                color: Color(0xff666666),
-              ),
-            ),
-          ))
-        ],
+      child: Text(
+        Strings.autoLineString(
+            'Estamos estimando su préstamo, Preste atención a los resultados de la revisión en la solicitud.'),
+        style: const TextStyle(
+          fontSize: 14.0,
+          color: Color(0xff666666),
+        ),
       ));
 }
